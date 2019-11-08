3549f348044bfd6d4230cbfebfe33865643e4497
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-11-08 16:53:46 -0300

    Estrutura Tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 import {
   IonCard,
   IonCardContent,
@@ -13,27 +15,71 @@ import {
   IonListHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonChip,
+  IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonButton
 } from '@ionic/react';
 
 import { book, build, colorFill, grid } from 'ionicons/icons';
-import React from 'react';
+
+import Male from '../../src/assets/male.png';
+
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Peso p/ Homem</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      
-      <IonContent>
-        
-      </IonContent>
-    </IonPage>
-  );
-};
+export default class Tab1 extends Component {
+  render() {
+    return (
 
-export default Tab1;
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Peso p/ Homem</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+
+          <IonGrid>
+            <IonRow>
+              <IonCol className="centerAvatar" size="12">
+                <IonAvatar>
+                  <img className="imgAvatar" src={Male} />
+                </IonAvatar>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonItem>
+                  <IonLabel position="floating">Digite sua altura(cm)</IonLabel>
+                  <IonInput id="valor1" value="" type="number"></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonButton onClick={() => this.pegaValores()} expand="block">Calcular</IonButton>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonInput placeholder="resultado" readonly type="number"></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+    );
+  };
+}
+
