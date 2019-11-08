bc05cb6a5f76d99ecb569c3dbf81865e5382bc7d
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-11-08 17:30:30 -0300

    Estrutura e Functions da Tab3

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,19 +1,96 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonPage, IonTitle, IonContent } from '@ionic/react';
+import React, { Component } from 'react';
 
-const Tab3Page: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Cálculo de IMC</IonTitle>
-        </IonToolbar>
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonButton
+} from '@ionic/react';
 
-      </IonHeader>
-      
-      <IonContent />
-    </IonPage>
-  );
-};
+import Saude from '../../src/assets/saude.png';
 
-export default Tab3Page;
+import './Tab1.css';
+
+export default class Tab3 extends Component {
+  
+  calcula2(altura: number, peso: number) {
+    console.log(altura)
+    let res = peso/(altura * altura);
+    res = parseFloat(res.toFixed(2));
+    document.querySelectorAll('input')[2].value = res.toString();
+  }
+
+  pegaAlturaPeso() {
+    let peso = parseFloat(document.querySelectorAll('input')[0].value);
+    let altura = parseFloat(document.querySelectorAll('input')[1].value);
+    this.calcula2(altura, peso);
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Cálculo de IMC</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+
+          <IonGrid>
+            <IonRow>
+              <IonCol className="centerAvatar" size="12">
+                <IonAvatar>
+                  <img className="imgAvatar" src={Saude} />
+                </IonAvatar>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonItem>
+                  <IonLabel position="floating">Digite seu peso(kg)</IonLabel>
+                  <IonInput id="valor1" value="" type="number"/>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonItem>
+                  <IonLabel position="floating">Digite sua altura(m)</IonLabel>
+                  <IonInput id="valor1" value="" type="number"/>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonButton onClick={() => this.pegaAlturaPeso()} expand="block">Calcular</IonButton>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonInput placeholder="resultado" readonly type="number"/>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+    );
+  };
+}
+
