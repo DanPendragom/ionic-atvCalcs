b1219bd63280fe377831dda93911530337093c77
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-11-08 17:05:58 -0300

    Estrutura e Functions da Tab2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,20 +1,85 @@
-import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { Component } from 'react';
 
-const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Peso p/ Mulher</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonAvatar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonButton
+} from '@ionic/react';
 
-      <IonContent>
-        
-      </IonContent>
-    </IonPage>
-  );
-};
+import Female from '../../src/assets/female.png';
 
-export default Tab2;
+import './Tab1.css';
+
+export default class Tab2 extends Component {
+  
+  calcula(altura: number) {
+    let res = (altura - 100) * 0.85 ;
+    
+    document.querySelectorAll('input')[1].value = res.toString();
+  }
+
+  pegaAlturaFemale() {
+    let altura = parseInt(document.querySelectorAll('input')[0].value);
+    this.calcula(altura);
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Peso p/ Mulher</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+
+          <IonGrid>
+            <IonRow>
+              <IonCol className="centerAvatar" size="12">
+                <IonAvatar>
+                  <img className="imgAvatar" src={Female} />
+                </IonAvatar>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonItem>
+                  <IonLabel position="floating">Digite sua altura(cm)</IonLabel>
+                  <IonInput id="valor1" value="" type="number"/>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol size="12">
+                <IonButton onClick={() => this.pegaAlturaFemale()} expand="block">Calcular</IonButton>
+              </IonCol>
+            </IonRow>
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonInput placeholder="resultado" readonly type="number"/>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
+          </IonGrid>
+        </IonContent>
+      </IonPage>
+    );
+  };
+}
+
